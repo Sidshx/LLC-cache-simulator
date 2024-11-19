7f97ee480740b9f8f9be96267ab51711d593240f
Siddesh Dinesh Patil <patilsid@mo.ece.pdx.edu>
2024-11-19 14:13:02 -0800

    Removed dependencies to Offset

--- checkpoint_2_data_structure/c_line.sv
+++ checkpoint_2_data_structure/c_line.sv
@@ -5,12 +5,14 @@ module cache (
 input line_st tag_array,
 input line_st mesi,
 input line_st valid,
-output wire [C_LINE_SIZE-1:0] offset,
+input line_st dirty,
+// output wire [C_LINE_SIZE-1:0] offset,
 output wire [N_WAY-1:0] index,
 output wire [ADDR_SIZE - C_LINE_SIZE - N_WAY-1:0] tag
 );
 
-assign offset = tag_array[C_LINE_SIZE-1 :0];
+// assign offset = tag_array[C_LINE_SIZE-1 :0];
+
 assign index = tag_array[C_LINE_SIZE+N_WAY-1 :C_LINE_SIZE];
 assign tag = tag_array[ADDR_SIZE+C_LINE_SIZE+N_WAY-1 :C_LINE_SIZE+N_WAY];
 
