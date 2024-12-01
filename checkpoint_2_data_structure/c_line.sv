beeb6950a8a280b97aa767bd06349b339368516e
Siddesh Dinesh Patil <patilsid@mo.ece.pdx.edu>
2024-11-30 17:28:13 -0800

    Modeled Line, Set and Cache

--- checkpoint_2_data_structure/c_line.sv
+++ checkpoint_2_data_structure/c_line.sv
@@ -2,16 +2,35 @@
 import line::*;
 
 module cache (
-input line::line_st tag_array,
-input line::line_st dirty,
-input line::line_st mesi,
-output wire [C_LINE_SIZE-1:0] offset,
-output wire [N_WAY-1:0] index,
-output wire [ADDR_SIZE - C_LINE_SIZE - N_WAY-1:0] tag
+input logic [ADDR_SIZE-1:0] address,
+output logic [INDEX_SIZE-1:0] index,
+output logic [TAG_SIZE-1:0] tag
+// output line_st cache_line_out
+
+// input line_st mesi,
+// input line_st valid,
+// input line_st dirty,
+// output wire [C_LINE_SIZE-1:0] offset,
 );
 
-assign offset = tag_array[C_LINE_SIZE-1 :0];
-assign index = tag_array[C_LINE_SIZE+N_WAY-1 :C_LINE_SIZE];
-assign tag = tag_array[ADDR_SIZE+C_LINE_SIZE+N_WAY-1 :C_LINE_SIZE+N_WAY];
+set_st cache_mem[NUM_SETS];
+
+// assign offset = tag_array[OFFSET_SIZE-1 :0];
+    task initialize_cache();
+        for (int i = 0; i < NUM_SETS; i++) begin
+            cache_mem[i].plru_bits = 0; 
+            for (int j = 0; j < N_WAY; j++) begin
+                cache_mem[i].ways[j].valid = 0;
+                cache_mem[i].ways[j].dirty = 0;
+                cache_mem[i].ways[j].tag = 0;
+            end
+        end
+    endtask
+
+
+assign index = address[OFFSET_SIZE+INDEX_SIZE-1 :OFFSET_SIZE];
+assign tag = address[ADDR_SIZE-1 :OFFSET_SIZE+INDEX_SIZE];
+
+//assign cache_line_out = cache_mem[index].ways[0];
 
 endmodule: cache
--- checkpoint_2_data_structure/pkg_line.sv
+++ checkpoint_2_data_structure/pkg_line.sv
@@ -1,19 +1,38 @@
 package line;
 
+parameter CACHE_SIZE = 16*1024*1024;
 parameter ADDR_SIZE =32; 
-parameter C_LINE_SIZE = 6; 
+parameter LINE_SIZE = 64;
+parameter OFFSET_SIZE = $clog2(LINE_SIZE); 
 parameter N_WAY = 16;
+parameter NUM_SETS = CACHE_SIZE / (LINE_SIZE * N_WAY);
+parameter INDEX_SIZE = $clog2(NUM_SETS);
+parameter TAG_SIZE = ADDR_SIZE - OFFSET_SIZE - INDEX_SIZE;
+
 
 typedef enum bit [1:0] {M, E, S, I} mesi_e;
 
 typedef struct packed {
-mesi_e mesi;
-bit dirty;
-reg [ADDR_SIZE-1:0] tag_array;
-// wire [C_LINE-1:0] offset;
-// wire [N_WAY-1:0] index;
-// wire [ADDR_SIZE - C_LINE - N_WAY-1:0] tag;
+	mesi_e mesi;
+	bit dirty;
+	bit valid;
+	// logic [ADDR_SIZE-1:0] tag_array;
+	// wire [C_LINE-1:0] offset;
+	// wire [N_WAY-1:0] index;
+	logic [TAG_SIZE-1:0] tag;
 } line_st;
 
-endpackage
+typedef struct {
+    logic [N_WAY-2:0] plru_bits;  // PLRU bits
+    line_st ways[N_WAY];  // Array of lines (ways) - this is packed
+} set_st;
+
+
+
+
+
+// set_st cache[]; // Array of sets
+
+
+endpackage : line
  
